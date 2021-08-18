a9e68531073e83d56b0a308531b57bceb4f7d40d
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-18 19:26:21 +0430

    home  html&css

---
+++ src/components/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+  return (
+    <main className='main'>
+      <section className='section-center'>
+        <h1>My submenu project</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+          sunt sit porro mollitia iusto aut ab? Nulla quisquam dolorum
+          laudantium adipisci, tempora officia eveniet reprehenderit error
+          facilis odit labore quam.
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default Home
