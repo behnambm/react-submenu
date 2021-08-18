23f11e2a95679883cabbcf5b9e48735cf9f45a99
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-18 19:28:51 +0430

    load all components in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Home from './components/Home'
+import Modal from './components/Modal'
+import Navbar from './components/Navbar'
+import Submenu from './components/Submenu'
+
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Navbar />
+      <Submenu />
+      <Home />
+      <Modal />
+    </div>
+  )
 }
 
 export default App
