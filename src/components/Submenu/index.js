dba7509b45a34179c16b2a508f06343eb0f1e557
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-18 19:28:21 +0430

    submenu html&css

---
+++ src/components/Submenu/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './submenu.css'
+
+function Submenu() {
+  return (
+    <div className='submenu show'>
+      Lorem ipsum dolor sit amet consectetc, rem molestias. Voluptate.
+    </div>
+  )
+}
+
+export default Submenu
