dabee3b5ce04aca0b8212017f5fdf2df61e3ce22
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-19 10:59:47 +0430

    complete Submenu

--- src/components/Submenu/index.js
+++ src/components/Submenu/index.js
@@ -9,9 +9,34 @@ function Submenu() {
   useEffect(() => {
     submenuRef.current.style.left = `${center}px`
   }, [center])
+
   return (
     <div className={`submenu ${isSubmenuOpen && 'show'}`} ref={submenuRef}>
-      {page.title}
+      <div className={page.title}>
+        {page.links.map((link, idx) => {
+          const { url, icon, label, img } = link
+
+          if (page.title === 'company') {
+            return (
+              <div className='item-wrapper'>
+                <img src={img} alt='' />
+                <a href={url} key={idx}>
+                  {icon}
+                  {label}
+                </a>
+              </div>
+            )
+          }
+          return (
+            <>
+              <a href={url} key={idx}>
+                {icon}
+                {label}
+              </a>
+            </>
+          )
+        })}
+      </div>
     </div>
   )
 }
