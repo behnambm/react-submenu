c4c7609c5b24c30461262842d955323f69938a3e
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-19 10:58:01 +0430

    update `data.js`

--- src/data.js
+++ src/data.js
@@ -1,31 +1,65 @@
-import { FaCreditCard, FaBook, FaBriefcase } from 'react-icons/fa'
+import {
+  FaCreditCard,
+  FaBook,
+  FaBriefcase,
+  FaLinux,
+  FaGithub,
+  FaGitlab,
+  FaServer,
+} from 'react-icons/fa'
+
+import { GoCode } from 'react-icons/go'
+import { BiPulse } from 'react-icons/bi'
+import { RiStackFill } from 'react-icons/ri'
+import { TiLocation } from 'react-icons/ti'
+
 import React from 'react'
+
+import img1 from './1.jpg'
+import img2 from './2.jpg'
+import img3 from './3.jpg'
+
 const sublinks = [
   {
     page: 'products',
     links: [
       { label: 'payment', icon: <FaCreditCard />, url: '/products' },
-      { label: 'terminal', icon: <FaCreditCard />, url: '/products' },
+      { label: 'terminal', icon: <FaGithub />, url: '/products' },
+      { label: 'connect', icon: <FaLinux />, url: '/products' },
       { label: 'connect', icon: <FaCreditCard />, url: '/products' },
+      { label: 'connect', icon: <FaGitlab />, url: '/products' },
     ],
   },
   {
     page: 'developers',
     links: [
-      { label: 'plugins', icon: <FaBook />, url: '/products', img: './1.jpg' },
-      {
-        label: 'libraries',
-        icon: <FaBook />,
-        url: '/products',
-        img: './2.jpg',
-      },
+      { label: 'Documents', icon: <GoCode />, url: '/products' },
+      { label: 'API Status', icon: <BiPulse />, url: '/products' },
+      { label: 'Platforms', icon: <RiStackFill />, url: '/products' },
+      { label: 'Servers', icon: <FaServer />, url: '/products' },
     ],
   },
   {
     page: 'company',
     links: [
-      { label: 'about', icon: <FaBriefcase />, url: '/products' },
-      { label: 'customers', icon: <FaBriefcase />, url: '/products' },
+      {
+        label: 'Los Angles, CA',
+        icon: <TiLocation />,
+        url: '/products',
+        img: img1,
+      },
+      {
+        label: 'New York, NY',
+        icon: <TiLocation />,
+        url: '/products',
+        img: img2,
+      },
+      {
+        label: 'Chicago, IL',
+        icon: <TiLocation />,
+        url: '/products',
+        img: img3,
+      },
     ],
   },
 ]
